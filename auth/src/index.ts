52fde22785c6a9d01251328de5ba8020664dbfc6
Thomasjoseph2 <tj089502@gmail.com>
2023-10-07 15:34:26 +0530

    db connected

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import 'express-async-errors'
 import { json } from "body-parser";
-
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -24,6 +24,19 @@ app.get('*',()=>{
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
+const start=async ()=>{
+ try{ 
+  await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+  console.log('db connected');
+  
+}
+ catch(err){
+  console.error(err);
+  
+ }
+ app.listen(3000, () => {
   console.log("Listening on port 3000!!!");
 });
+}
+
+start();
